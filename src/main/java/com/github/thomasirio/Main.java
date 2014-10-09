ea4f8b57483d46787f2fa3523449d20f8935b3f1
Tommaso De Gennaro <tommasodegennaro@libero.it>
2014-10-09 08:25:36 +0000

    Push 9-10-2014

---
+++ src/main/java/com/github/thomasirio/Main.java
@@ -0,0 +1,11 @@
+package com.github.thomasirio;
+
+/**
+ * Created by Tommaso on 09/10/2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("main!");
+    }
+}
